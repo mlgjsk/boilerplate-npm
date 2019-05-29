38e91dc85692645e15ee3d5e1f713a5ef5ec3fb4
mlgjsk <buba_malogajski@outlook.com>
2019-05-29 16:17:38 +0200

    Basic methods added to App.js

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 var express = require('express');
 var app = express();
+// --> 7)  Mount the Logger middleware here
+
+// --> 11)  Mount the body-parser middleware  here
+
+/** 1) Meet the node console. */
+console.log('Hello World');
+
+/** 2) A first working Express Server */
+app.get('/', function(req, res) {
+	res.sendFile('/views/index.html', { root: __dirname });
+});
+
+/** 3) Serve an HTML file */
+app.use(express.static(__dirname + '/public'));
+/** 4) Serve static assets  */
+
+/** 5) serve JSON on a specific route */
+/*app.get('/json', function(req, res){
+  res.json({"message": "Hello json"});
+})
+*/
